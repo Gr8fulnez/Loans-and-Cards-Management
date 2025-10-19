eb4e0412554676ddeb8ae917fa63740962d01e8d
john oluwole <oluwolejohn72@gmail.com>
2025-10-19 02:54:55 +0100

    feat:swagger doc update

--- src/main/java/com/gr8fulnez/accounts/AccountsApplication.java
+++ src/main/java/com/gr8fulnez/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package com.gr8fulnez.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+            title = "Accounts Microservice API Documentation",
+            description = "Gr8fulnez Account microservice REST API documentation",
+            version = "v1",
+                contact = @Contact(
+                    name = "John Oluwole",
+                    email = "oluwolejohn72@gmail.com",
+                    url = "https://www.linkedin.com/in/john-gr8fulnez/"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Accounts Microservice Documentation",
+                url = "https://github.com/Gr8fulnez/Loans-and-Cards-Management"
+        )
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
